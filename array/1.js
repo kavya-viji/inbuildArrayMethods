73159fe5adaba4f1b3fce9c791c50fd8e71f6f46
Admin <kavyak6999@gmail.com>
2022-12-17 10:36:09 +0530

    array methods

---
+++ array/1.js
@@ -0,0 +1,136 @@
+var x = [1,2,3,4,5,6];
+x.push(7);
+alert (x);
+
+// var x = [1,2,3,4,5,6];
+// y = x.push(7);
+// alert (y);
+
+//var a = ["cat","goat","zebra","donkey","buffaloo"];
+//a.shift()
+//alert (a)
+
+// var a = ["cat","goat","zebra","donkey","buffaloo"];
+// b = a.shift();
+// alert (b);
+
+//var a = ["cat","goat","zebra","donkey","buffaloo"];
+//a.shift()
+//alert (a)
+
+// var a = ["cat","goat","zebra","donkey","buffaloo"];
+// b = a.shift();
+// alert (b);
+
+//var a = ["cat","goat","zebra","donkey","buffaloo"];
+//a.unshift("camel")
+//alert (a)
+
+// var a = ["cat","goat","zebra","donkey","buffaloo"];
+// b = a.unshift("camel")
+// alert (b)
+
+// var a = ["carrot","tomato","beetroot","raddish","onion"];
+// a.splice(5,2,"brinjal","capsicum")
+// alert (a)
+
+// var a = ["carrot","tomato","beetroot","raddish","onion"];
+//b = a.splice(5,2,"brinjal","capsicum")
+//alert (b)
+
+//var a = ["carrot","tomato","beetroot","raddish","onion"];
+//a.splice(2,2)
+//alert (a)
+
+//var a = ["carrot","tomato","beetroot","raddish","onion"];
+//b = a.splice(2,2)
+//alert (b)
+
+ //var a = ["carrot","tomato","beetroot","raddish","onion"];
+// a.splice(3,2,"brinjal","capsicum")
+ //alert (a)
+
+// var a = ["carrot","tomato","beetroot","raddish","onion"];
+// b = a.splice(3,2,"brinjal","capsicum")
+// alert (b)
+
+//---1---
+// function mainFunction(getcallfunction)
+// {
+// console.log("Main function Executed");
+// }
+// function callBack()
+// {
+// console.log("callBack function Executed");
+// }
+//
+// mainFunction(callBack);
+
+//---2---
+// function add(a,b)
+// {
+// 	console.log(a+b);
+// }
+//  add (10,20)
+
+//---3---
+// function add(a,b)
+// {
+// 	return a+b;
+// }
+// document.write(add (110,220));
+
+//---4---
+// let age = [23,45,78,13,29];
+// var x=age.filter(function(value)
+// {
+// 	return value>=25
+// });
+// document.write(x);
+
+//----5-filter----
+// var num = [0,1,2,3,4,5,6,7,8,9];
+// let even = num.filter(function(value)
+// {
+// 	return value %2==0
+// });
+//
+// let odd = num.filter(function(value)
+// {
+// 	return value %2!==0
+// });
+// document.write("Even nos:",even)
+// document.write("<br>")
+// document.write("Odd nos:",odd);
+
+//---6-find---
+// var num = [11,17,2,3,4,5,6,7,8,9];
+// let even = num.findIndex(function(value)
+// {
+// 	return value %2==0
+// });
+//
+// let odd = num.findIndex(function(value)
+// {
+// 	return value %2!==0
+// });
+// document.write("Even nos:",even)
+// document.write("<br>")
+// document.write("Odd nos:",odd);
+
+// ---7-map---
+// var a = [1,2,3,4,5,6,7];
+// var x = a.map(function(value)
+// {
+// 	return value*5;
+// });
+// document.write(x);
+
+
+
+
+
+
+
+
+
